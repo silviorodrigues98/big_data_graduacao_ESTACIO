2a462801b325cfc4edc18c120e4f45a8192f9256
Silvio Rodrigues Correa <silviorodrigues98@hotmail.com>
2024-08-28 22:34:21 -0300

    refactor: Detecção facial atualizada e tratamento de erros em detect.py

--- detect.py
+++ detect.py
@@ -1,12 +1,13 @@
-from deepface import DeepFace
 import cv2
 import logging
+from deepface import DeepFace
 
 def configurar_log():
     logging.basicConfig(filename='log_reconhecimento_facial.txt', level=logging.INFO, 
                         format='%(asctime)s - %(levelname)s - %(message)s')
 
 def registrar_log(mensagem, nivel='info'):
+    print(f"LOG: {mensagem}")
     if nivel == 'info':
         logging.info(mensagem)
     elif nivel == 'erro':
@@ -14,26 +15,24 @@ def registrar_log(mensagem, nivel='info'):
     elif nivel == 'aviso':
         logging.warning(mensagem)
 
-# Lista de backends, modelos e métricas de distância disponíveis
-backends = ["opencv", "ssd", "dlib", "mtcnn", "retinaface"]
-modelos = ["VGG-Face", "Facenet", "Facenet512", "OpenFace", "DeepFace", "DeepID", "ArcFace", "Dlib", "SFace"]
-metricas = ["cosine", "euclidean", "euclidean_l2"]
-
 def reconhecimento_facial_tempo_real():
     # Definir um objeto de captura de vídeo
     video = cv2.VideoCapture(0)
     registrar_log("Captura de vídeo iniciada.")
 
+    cv2.namedWindow('Reconhecimento Facial', cv2.WINDOW_NORMAL)
+
     while True:
         # Capturar o quadro do vídeo frame a frame
-        ret, quadro = video.read()
+        ret, frame = video.read()
         if not ret:
             registrar_log("Erro: Não foi possível capturar o quadro de vídeo", nivel='erro')
             break
         
         # Encontrar rostos e identificar pessoas usando um modelo específico e métrica de distância
         try:
-            pessoas = DeepFace.find(img_path=quadro, db_path="Data/", model_name=modelos[2], distance_metric=metricas[2], enforce_detection=False)
+            img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            pessoas = DeepFace.find(img_path=img, db_path="Data/", model_name="VGG-Face", distance_metric="cosine", enforce_detection=False)
             registrar_log(f"{len(pessoas)} rostos detectados.")
         except Exception as e:
             registrar_log(f"Erro: Não foi possível abrir o banco de dados. Detalhes: {str(e)}", nivel='erro')
@@ -52,27 +51,25 @@ def reconhecimento_facial_tempo_real():
                 h = pessoa['source_h'][0]
 
                 # Desenhar um retângulo ao redor do rosto
-                cv2.rectangle(quadro, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
                 # Obter o nome da pessoa e exibi-lo na imagem
                 nome = pessoa['identity'][0].split('/')[1]
-                cv2.putText(quadro, nome, (x, y), cv2.FONT_ITALIC, 1, (0, 0, 255), 2)
+                cv2.putText(frame, nome, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
                 registrar_log(f"Rosto identificado: {nome}")
             except Exception as e:
                 registrar_log(f"Erro ao processar rosto detectado. Detalhes: {str(e)}", nivel='erro')
-                pass
+                pass 
         
         # Exibir o quadro resultante
-        cv2.namedWindow('RECONHECIMENTO FACIAL', cv2.WINDOW_NORMAL)
-        cv2.resizeWindow('RECONHECIMENTO FACIAL', 640, 480)
-        cv2.imshow('RECONHECIMENTO FACIAL', quadro)
+        cv2.imshow('Reconhecimento Facial', frame)
 
         # Verificar se o botão 'q' foi pressionado para sair do programa
-        if cv2.waitKey(100) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             registrar_log("Encerrando captura de vídeo.")
             break
 
-    # Liberar o objeto de captura de vídeo e fechar todas as janelas
+    # Liberar o objeto de captura de vídeo
     video.release()
     cv2.destroyAllWindows()
     registrar_log("Sistema de reconhecimento facial encerrado.")
